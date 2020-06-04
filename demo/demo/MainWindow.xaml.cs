e0fe5dbdb9d37e59cc232a0841c722d058743bd6
Alaa <9027148+alaabenfatma@users.noreply.github.com>
2020-06-05 00:02:40 +0200

    demo : added back code & GUI

--- demo/demo/MainWindow.xaml.cs
+++ demo/demo/MainWindow.xaml.cs
@@ -16,13 +16,50 @@ using System.Windows.Shapes;
 namespace demo
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    ///     Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            Closed += (s, e) => { Application.Current.Shutdown(); };
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var basic = new Basic();
+            basic.ShowDialog();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            var vars = new VarsList();
+            vars.ShowDialog();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            var saveload = new SaveLoad();
+            saveload.ShowDialog();
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            var stan = new Standard();
+            stan.ShowDialog();
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            var perf = new Performance();
+            perf.ShowDialog();
+        }
+
+        private void Button_Click_5(object sender, RoutedEventArgs e)
+        {
+            var advanced = new Advanced();
+            advanced.ShowDialog();
         }
     }
 }

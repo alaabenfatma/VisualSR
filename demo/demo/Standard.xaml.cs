91c38df5a6b8a27fb077924e0835c86940250a93
Alaa <9027148+alaabenfatma@users.noreply.github.com>
2020-06-05 00:10:18 +0200

    demo : added ref to winforms + Standard sample

---
+++ demo/demo/Standard.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Windows;
+using System.Windows.Forms;
+using VisualSR.Compiler;
+using VisualSR.Tools;
+
+namespace demo
+{
+    /// <summary>
+    ///     Interaction logic for SaveLoad.xaml
+    /// </summary>
+    public partial class Standard : Window
+    {
+        public Standard()
+        {
+            Closing += Standard_Closing;
+
+            InitializeComponent();
+            var browser = new FolderBrowserDialog();
+            var path = "";
+            if (browser.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                path = browser.SelectedPath + @"\";
+            else
+                Close();
+            Hub.WorkSpace = path;
+            vars.Host = vc;
+            compile.Click += (s, e) => Compile();
+        }
+
+        private void Standard_Closing(object sender, CancelEventArgs e)
+        {
+            e.Cancel = true;
+            Visibility = Visibility.Hidden;
+        }
+
+        private void Compile()
+        {
+            File.WriteAllText(Hub.WorkSpace + "code.r", CodeMiner.Code(vc.RootNode));
+            var path = Hub.WorkSpace + "virtualConsole.bat";
+            var cmd = @"@echo off
+title Console
+rscript """ + Hub.WorkSpace + @"code.r""" + @" 
+
+pause";
+
+            File.WriteAllText(path, cmd);
+            Process.Start(path);
+        }
+    }
+}

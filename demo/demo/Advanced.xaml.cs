6539a59b11d94a23c02c4b4907db80a069bcfb77
Alaa <9027148+alaabenfatma@users.noreply.github.com>
2020-06-05 00:27:40 +0200

    added advanced & gauge

---
+++ demo/demo/Advanced.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Windows;
+using System.Windows.Forms;
+using VisualSR.Compiler;
+using VisualSR.Tools;
+
+
+namespace demo
+{
+    /// <summary>
+    /// Interaction logic for Advanced.xaml
+    /// </summary>
+    public partial class Advanced : Window
+    {
+        public Advanced()
+        {
+            Closing += Advanced_Closing;
+            InitializeComponent();
+            vars.Host = vc;
+            var browser = new FolderBrowserDialog();
+            var path = "";
+            if (browser.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                path = browser.SelectedPath + @"\";
+            else
+                Close();
+            Hub.WorkSpace = path;
+
+            compile.Click += (s, e) => Compile();
+        }
+
+        private void Advanced_Closing(object sender, CancelEventArgs e)
+        {
+            e.Cancel = true;
+            Visibility = Visibility.Hidden;
+        }
+
+        private void Compile()
+        {
+            File.WriteAllText(Hub.WorkSpace + "code.r", CodeMiner.Code(vc.RootNode));
+            var path = Hub.WorkSpace + "virtualConsole.bat";
+            var cmd = @"@echo off
+title Console
+rscript """ + Hub.WorkSpace + @"code.r""" + @" 
+
+pause";
+
+            File.WriteAllText(path, cmd);
+            Process.Start(path);
+        }
+    }
+}

943e7dfc9730ae6f6f26a9ad06e154d842a6e468
Alaa <9027148+alaabenfatma@users.noreply.github.com>
2020-06-05 21:27:25 +0200

    node ; Greater or Equal

---
+++ Nodes/Nodes/Nodes/Logic/GreaterEqual.cs
@@ -0,0 +1,69 @@
+﻿using System.ComponentModel.Composition;
+using VisualSR.Core;
+using VisualSR.Tools;
+
+namespace Nodes.Nodes.Logic
+{
+    [Export(typeof(Node))]
+    public class GreaterEqual : Node
+
+    {
+        private readonly VirtualControl Host;
+
+        [ImportingConstructor]
+        public GreaterEqual([Import("host")] VirtualControl host,
+            [Import("bool")] bool spontaneousAddition = false) : base(host, NodeTypes.Basic, spontaneousAddition)
+        {
+            Title = ">=";
+
+            Category = "Logic nodes";
+            Host = host;
+            AddObjectPort(this, "", PortTypes.Input, RTypes.Generic, false);
+            AddObjectPort(this, "", PortTypes.Input, RTypes.Generic, false);
+            AddObjectPort(this, ">=", PortTypes.Output, RTypes.Logical, true);
+            MouseRightButtonDown += (sender, args) => GenerateCode();
+            foreach (var port in InputPorts)
+                port.DataChanged += (sender, args) => { GenerateCode(); };
+            InputPorts[1].LinkChanged += (sender, args) =>
+            {
+                if (!InputPorts[1].Linked)
+                    InputPorts[1].Data.Value = "";
+            };
+            InputPorts[0].LinkChanged += (sender, args) =>
+            {
+                if (!InputPorts[0].Linked)
+                    InputPorts[0].Data.Value = "";
+            };
+        }
+
+        public GreaterEqual()
+        {
+        }
+
+
+        public override string GenerateCode()
+        {
+            OutputPorts[0].Data.Value = "((" + InputPorts[0].Data.Value + ")>=(" + InputPorts[1].Data.Value + "))";
+            return OutputPorts[0].Data.Value;
+        }
+
+        public override Node Clone()
+        {
+            var node = new GreaterEqual(Host, false);
+
+            return node;
+        }
+
+        public override string Serialize()
+        {
+            NodeProperties = new NodeProperties(this);
+            Cipher.SerializeToFile(@"sum.txt", NodeProperties, true);
+            return null;
+        }
+
+        public override string DeSerialize()
+        {
+            return null;
+        }
+    }
+}

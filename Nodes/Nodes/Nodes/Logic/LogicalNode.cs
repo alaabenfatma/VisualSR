199456804fb81ab764fc536c20d74dc96559521b
Alaa <9027148+alaabenfatma@users.noreply.github.com>
2020-06-05 21:29:49 +0200

    node : boolean

---
+++ Nodes/Nodes/Nodes/Logic/LogicalNode.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.Composition;
+using VisualSR.Controls;
+using VisualSR.Core;
+
+namespace Nodes.Nodes.Logic
+{
+    [Export(typeof(Node))]
+    public class LogicalNode : Node
+    {
+        private readonly UnrealControlsCollection.CheckBox _cb = new UnrealControlsCollection.CheckBox();
+        private readonly VirtualControl Host;
+
+        [ImportingConstructor]
+        public LogicalNode([Import("host")] VirtualControl host,
+            [Import("bool")] bool spontaneousAddition = false) : base(host, NodeTypes.Basic,
+            spontaneousAddition)
+        {
+            Title = "Bool";
+            Host = host;
+
+            Category = "Logic nodes";
+            AddObjectPort(this, "return value", PortTypes.Output, RTypes.Logical, true, _cb);
+            _cb.GotFocus += (sender, args) =>
+            {
+                _cb.IsChecked = !_cb.IsChecked;
+                OutputPorts[0].Data.Value = _cb.IsChecked.Value.ToString();
+            };
+        }
+
+        public override string GenerateCode()
+        {
+            return null;
+        }
+
+        public override Node Clone()
+        {
+            var node = new LogicalNode(Host, false);
+
+            return node;
+        }
+    }
+}
